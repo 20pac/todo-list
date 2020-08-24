d4432d0d56bf6c829ea831dfba84decd8b887ecf
skydogcat23 <skydogcat23@gmail.com>
2020-08-24 21:54:13 +0530

    Updates

---
+++ precache-manifest.aa9b1ba7a410ff66279e45334dcf6bea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "daa52702ecbec0ed6ff06e2e9bf53e56",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "e2289f0e650928884424",
+    "url": "/todo-list/static/css/main.39088bb4.chunk.css"
+  },
+  {
+    "revision": "b570a59c7a8c70900f1b",
+    "url": "/todo-list/static/js/2.faab6034.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-list/static/js/2.faab6034.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e2289f0e650928884424",
+    "url": "/todo-list/static/js/main.240fe79f.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
